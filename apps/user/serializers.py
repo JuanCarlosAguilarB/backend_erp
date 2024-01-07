5a028c71bb58abf3156a7bc5d68735bbaac268d0
Juan Carlos A <abjuancarlos.12@gmail.com>
2024-01-07 15:23:11 -0500

    fix

--- apps/user/serializers.py
+++ apps/user/serializers.py
@@ -16,17 +16,11 @@ class ListUserSerializer(serializers.ModelSerializer):
     """
     serializers for list all users
     """
-    full_name = serializers.SerializerMethodField()
 
     class Meta:
         model = User
         exclude = ('password', 'user_permissions', 'is_superuser',
                    'last_login', 'is_staff', 'is_active', 'groups')
-
-    def get_full_name(self, obj):
-        first_name = obj.first_name or ''
-        last_name = obj.last_name or ''
-        return f'{first_name} {last_name}'
 
 
 class CreateUserSerializer(serializers.ModelSerializer):
